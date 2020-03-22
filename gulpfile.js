e91b6b9b6b6e140fae26a899afc3ff97d2c4901f
Meta <meta012@gmail.com>
2020-03-22 03:33:44 +0200

    Added base structure

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+// terminal -> gulp watch
+// must have a body in html file
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// Compile SCSS into CSS & inject into browsers
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+}
+
+// Static server & watch scss + html files
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: 'src/'
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('src/*.html').on('change',browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
